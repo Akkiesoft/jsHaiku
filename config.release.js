2b0ab52810e20d86c8d794e6ca709a921dac5d88
Akira Ouchi <akkiesoft@marokun.net>
2012-08-20 03:04:06 +0900

    設定ファイルわけた

---
+++ config.release.js
@@ -0,0 +1,8 @@
+var options;
+options = {
+    consumerKey: "",
+    consumerSecret: "",
+    requestTokenUrl: "https://www.hatena.com/oauth/initiate",
+    authorizationUrl: "https://www.hatena.ne.jp/oauth/authorize",
+    accessTokenUrl: "https://www.hatena.com/oauth/token"
+};
